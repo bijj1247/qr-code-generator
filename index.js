a43f97f6323ac8e4d48aaf6dfdd3cd5d0103d624
B.Sai Ganesh <saiganeshbijjala6@gmail.com>
2024-10-17 22:26:52 +0530

    new commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
-// hemant is a bitch
+// hemant is not a good boy
 inquirer
   .prompt([
     {
-        "message":"Type in your URL:",
-        "name":"URL"
-    }
+      message: "Type in your URL:",
+      name: "URL",
+    },
   ])
   .then((answers) => {
     const url = answers.URL;
@@ -15,11 +15,10 @@ inquirer
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("quiz_img.png"));
 
-    fs.writeFile("URL-quiz.txt",url,(err)=>{
+    fs.writeFile("URL-quiz.txt", url, (err) => {
       if (err) throw err;
       console.log("succesfully inserted");
-    })
-
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
